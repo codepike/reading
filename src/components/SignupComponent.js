c94e55c66a2ed5a0e3bf350489dadbb611327bc0
codepike@gmail.com <codepike@gmail.com>
2018-09-15 12:19:21 -0400

    updated sign up

--- src/components/SignupComponent.js
+++ src/components/SignupComponent.js
@@ -1,55 +1,47 @@
 
 import React from 'react';
 import { Input, Button, Container, Form, FormGroup, Label, Modal,
-          ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+          ModalBody, ModalFooter, ModalHeader, Row, Col } from 'reactstrap';
 import './signup.css';
+
 
 const Signup = (props) => {
 
   return (
     <Container className='App'>
-      <Form >
-        <FormGroup>
-          <Label for="username">Username</Label>
-          <Input type="text" name="username" id="username" placeholder="Enter your username" />
-        </FormGroup>
+      <Col sm={{ size: 6, order: 2, offset: 3 }}>
+          <Form >
+            <FormGroup>
+              <Label for="username">Username</Label>
+              <Input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Enter your username"
+                onChange = {props.handleUserInputChange}/>
+            </FormGroup>
 
-        <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-        </FormGroup>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="password placeholder"
+                onChange = {props.handleUserInputChange}/>
+            </FormGroup>
 
-        <FormGroup>
-          <Label for="email">Email</Label>
-          <Input type="email" name="email" id="email" placeholder="Enter your email" />
-        </FormGroup>
-     </Form>
-
-     <Modal isOpen={true} toggle={props.toggle} className={props.className}>
-  <ModalHeader toggle={props.toggle}>Modal title</ModalHeader>
-  <ModalBody>
-  <Form >
-    <FormGroup>
-      <Label for="username">Username</Label>
-      <Input type="text" name="username" id="username" placeholder="Enter your username" />
-    </FormGroup>
-
-    <FormGroup>
-      <Label for="examplePassword">Password</Label>
-      <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-    </FormGroup>
-
-    <FormGroup>
-      <Label for="email">Email</Label>
-      <Input type="email" name="email" id="email" placeholder="Enter your email" />
-    </FormGroup>
- </Form>
-  </ModalBody>
-  <ModalFooter>
-    <Button color="primary" onClick={props.toggle}>Do Something</Button>{' '}
-    <Button color="secondary" onClick={props.toggle}>Cancel</Button>
-  </ModalFooter>
-</Modal>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+                onChange = {props.handleUserInputChange}/>
+            </FormGroup>
+            </Form>
+        </Col>
     </Container>
   );
 };
